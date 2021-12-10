0602b43357afaf83c833d116e49287b922ac864c
Thrane <einheriii@gmail.com>
2021-12-10 17:49:55 +0100

    Initial code commit..

---
+++ src/main/java/com/alpha/util/cert/CaCertGenerator.java
@@ -0,0 +1,21 @@
+package com.alpha.util.cert;
+
+/*
+ * Utility which support generation of CA root and intermediate certificates.
+ */
+public class CaCertGenerator {
+	
+	/**
+	 * No-arg constructor.
+	 */
+	CaCertGenerator () {		
+	}
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+        
+    public static CaCertGenerator getInstance() {
+    	return new CaCertGenerator();
+    }
+}
